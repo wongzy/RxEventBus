f3c7a1efa9a06ccf2662eecb4b980efbd739df32
JoshuaRogue <790509558@qq.com>
2018-01-31 13:06:16 +0800

    2018131

---
+++ processors/src/main/java/site/gemus/processors/DefaultJavaCode.java
@@ -0,0 +1,42 @@
+package site.gemus.processors;
+
+import javax.lang.model.element.TypeElement;
+
+/**
+ * @author Jackdow
+ * @version 1.0
+ *          RxEventBus
+ */
+
+class DefaultJavaCode extends GenerateJavaCode {
+
+    @Override
+    String addPackage() {
+        return null;
+    }
+
+    @Override
+    String addImport() {
+        return null;
+    }
+
+    @Override
+    String addClassName() {
+        return null;
+    }
+
+    @Override
+    String addMethodNameAndStart() {
+        return null;
+    }
+
+    @Override
+    String addMethodBody(TypeElement element) {
+        return null;
+    }
+
+    @Override
+    String addMethodEnd() {
+        return null;
+    }
+}
